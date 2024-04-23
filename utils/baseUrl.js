0ca99070ca03554344523d8a07f09c304a963f81
gurdeep1230 <gurisachin09@gmail.com>
2024-04-23 20:29:57 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "https://elearniv-react.envytheme.com"
+		? "https://elearniv-react-next-i5p7.vercel.app"
 		: "http://localhost:3000";
 
 export default baseUrl;
